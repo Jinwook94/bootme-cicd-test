7dbe94e49a5a831751889afeef0deccfdcfd2125
Jinwook Kim <kimjinwook94@gmail.com>
2023-03-11 23:34:30 +0900

    정규식 맨 처음 것으로 다시 변경함
    jasypt 값을 아예 변경: $를 제거시

--- backend/src/main/java/com/bootme/BootmeApplication.java
+++ backend/src/main/java/com/bootme/BootmeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BootmeApplication {
-    // Test ha g함dh
+    // Test ha g함dh 다시
     public static void main(String[] args) {
         SpringApplication.run(BootmeApplication.class, args);
     }

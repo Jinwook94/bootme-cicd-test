5adf51f6bbcbf256ff6521589ec1ee13de374348
Jinwook Kim <kimjinwook94@gmail.com>
2023-03-11 21:37:35 +0900

    feat: appspec.yml 오타 수정 등

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 REPOSITORY=/home/ec2-user/deployment
 cd $REPOSITORY
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep "SNAPSHOT.jar" | tail -n 1)
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep "jar" | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 echo "> $JAR_PATH 배포"
 nohup java -jar $JAR_PATH > /home/ec2-user/deployment/logfile.log 2>&1 &

e5458d762c9126cf9a36b39f4f7b3dd72c2d58f7
Jinwook Kim <kimjinwook94@gmail.com>
2023-03-13 03:40:49 +0900

    워크플로우 파일 최종 변경

--- frontend/webpack/webpack.common.js
+++ frontend/webpack/webpack.common.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require('path');
-const webpack = require("webpack");
+const webpack = require('webpack');
 
 module.exports = {
   entry: {

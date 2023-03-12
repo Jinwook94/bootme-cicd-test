eaf800aecf784f6baacc4bc555f457f20a93557c
Jinwook Kim <kimjinwook94@gmail.com>
2023-03-13 03:21:54 +0900

    feat: 테스트 페이지 빈페이지로

--- frontend/src/pages/Test/index.tsx
+++ frontend/src/pages/Test/index.tsx
@@ -1,41 +1,5 @@
-import { SecretsManagerClient, GetSecretValueCommand } from '@aws-sdk/client-secrets-manager';
-import React from 'react';
-
-// 테스
-
-const access_key: string = process.env.AWS_SDK_CREDENTIAL_ACCESS_KEY!;
-const secret_key: string = process.env.AWS_SDK_CREDENTIAL_SECRET_KEY!;
-
 const Test = () => {
-  const client = new SecretsManagerClient({
-    region: 'ap-northeast-2',
-    credentials: {
-      accessKeyId: access_key,
-      secretAccessKey: secret_key,
-    },
-  });
-
-  const secret_name = 'prod/React';
-
-  const getSecrets = async () => {
-    try {
-      const response = await client.send(
-        new GetSecretValueCommand({
-          SecretId: secret_name,
-          VersionStage: 'AWSCURRENT',
-        })
-      );
-      console.log(response);
-      const secrets = JSON.parse(response.SecretString as string);
-      console.log(secrets['api-url']);
-    } catch (error) {
-      console.log(error);
-      // For a list of exceptions thrown, see
-      // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-      throw error;
-    }
-  };
-  return <div onClick={() => getSecrets()}>Secrets</div>;
+  return <></>;
 };
 
 export default Test;

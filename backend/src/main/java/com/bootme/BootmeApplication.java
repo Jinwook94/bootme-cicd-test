f6dda9ef24a569cb3b19a055711505e892f476a2
Jinwook Kim <kimjinwook94@gmail.com>
2023-03-13 19:32:57 +0900

    백엔드 CICD 테스트

--- backend/src/main/java/com/bootme/BootmeApplication.java
+++ backend/src/main/java/com/bootme/BootmeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BootmeApplication {
-    // 테스트
+    // Test
     public static void main(String[] args) {
         SpringApplication.run(BootmeApplication.class, args);
     }

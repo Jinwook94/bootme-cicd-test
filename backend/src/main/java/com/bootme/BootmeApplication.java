6054fca664fdfcc0253b7b04f417b6c7d59998cb
Jinwook Kim <kimjinwook94@gmail.com>
2023-03-16 15:55:32 +0900

    PR 댓글로 테스트 결과 달리는지 테스트 6

--- backend/src/main/java/com/bootme/BootmeApplication.java
+++ backend/src/main/java/com/bootme/BootmeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BootmeApplication {
-    // Test
+    // Test 테스트
     public static void main(String[] args) {
         SpringApplication.run(BootmeApplication.class, args);
     }
